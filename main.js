16618f03969f1a61c75658490ec478fe9aa5612e
Milad <108275659+Milad-exe@users.noreply.github.com>
2025-10-01 23:58:44 +0800

    [Add] added tabs

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-const { BrowserWindow, app, WebContentsView }  = require('electron');
+const { BrowserWindow, app, ipcMain }  = require('electron');
+const path = require("path");
+const Tabs = require("./Features/tabs")
 
 //the actual browser
 class Ink {
     constructor() {
         this.mainWindow = null;
+        this.tabs = null;
 
         this.Init();
     }
 
     Init(){
+
         const createWindow = () => {
             this.mainWindow = new BrowserWindow({
                 width: 800,
-                height: 600
+                height: 600,
+                minWidth: 800,
+                minHeight: 600,
+                webPreferences: {
+                    preload: path.join(__dirname, "preload.js"),
+                }
             })
 
-            this.mainWindow.loadFile('renderer/index.html')
-
-
-
-            const view1 = new WebContentsView()
-            this.mainWindow.contentView.addChildView(view1)
-            view1.webContents.loadURL('https://electronjs.org')
-            view1.setBounds({ x: 0, y: 0, width: 400, height: 400 })
+            this.mainWindow.loadFile('renderer/Browser/index.html')
+            
+            this.tabs = new Tabs(this.mainWindow)
+            
+            // Create initial tab when window is ready
+            this.mainWindow.webContents.once('did-finish-load', () => {
+                this.tabs.CreateTab()
+            })
         }
+
+
         app.whenReady().then(() => {
             createWindow()
             app.on('activate', () => {
@@ -31,6 +42,10 @@ class Ink {
                     createWindow()
                 }
             })
+
+            if(this.tabs.TabMap.size == 0){
+                this.tabs.CreateTab()
+            }
         })
         app.on('window-all-closed', () => {
             if (process.platform !== 'darwin') app.quit()
@@ -40,4 +55,21 @@ class Ink {
     
 }
 
-new Ink();
+const inkInstance = new Ink();
+
+//IPC HANDLING
+ipcMain.handle("addTab", async () => {
+  inkInstance.tabs.CreateTab()
+});
+
+ipcMain.handle("removeTab", async (event, index) => {
+  inkInstance.tabs.removeTab(index)
+});
+
+ipcMain.handle("switchTab", async (event, index) => {
+  inkInstance.tabs.showTab(index)
+});
+
+ipcMain.handle("loadUrl", async (event, index, url) => {
+  inkInstance.tabs.loadUrl(index, url)
+});
